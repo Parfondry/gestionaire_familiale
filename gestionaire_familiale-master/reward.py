5aca6b4d289e5dcb3f9c355bea3b54da2bed009b
Parfondry <64077936+Parfondry@users.noreply.github.com>
2023-01-12 22:28:49 +0100

    property manquantes

--- gestionaire_familiale-master/reward.py
+++ gestionaire_familiale-master/reward.py
@@ -1,5 +1,15 @@
 
 class Reward:
 
-    def __init__(self):
-        pass
+    def __init__(self, cost_point, reward_name):
+        
+        self.__cost_point = cost_point
+        self.__reward_name = reward_name
+    
+    @property
+    def cost_point(self):
+        return self.__cost_point
+
+    @property
+    def name(self):
+        return self.__reward_name
