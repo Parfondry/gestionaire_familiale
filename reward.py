c662d2ffa4aaf636fd649e53730563d29d5e616c
FirminMoyen <firminmoyen@gmail.com>
2023-08-12 16:07:00 +0200

    removing unnecesary files and cleaning the structure of the project

--- reward.py
+++ reward.py
@@ -1,28 +1,28 @@
 
 class Reward:
 
-    def __init__(self, cost_point, name):
-        
-        self.__cost_point = cost_point
+    def __init__(self, cost, name):
+
+        self.__cost_point = cost
         self.__name = name
-    
+
     @property
-    def cost_point(self):
-        return self.__cost_point
+    def cost(self):
+        return self.__cost
 
     @property
     def name(self):
         return self.__name
 
-    @cost_point.setter
-    def cost_point(self,cost_point):
-        self.__cost_point=cost_point
+    @cost.setter
+    def cost(self, cost):
+        self.__cost = cost
 
     @name.setter
-    def name(self,name):
-        self.__name=name
+    def name(self, name):
+        self.__name = name
 
-    #fonction qui permet de transformer un objet TASK en un dictionaire.
-    def transform_to_dico(self):
-        dico={'name':self.name,'cost_point':self.cost_point}     
-        return dico
+    # #fonction qui permet de transformer un objet TASK en un dictionaire.
+    # def transform_to_dico(self):
+    #     dico={'name':self.name,'cost_point':self.cost_point}
+    #     return dico
