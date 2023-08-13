d8a9a0adbd1b738a1c96ec38ec85e220ad3cb599
hheunen <45571110+hheunen@users.noreply.github.com>
2023-08-13 01:48:43 +0000

    ajout des methodes de modif/add/supp dans la classe datamanager

--- datamanager.py
+++ datamanager.py
@@ -62,6 +62,85 @@ class DataManager():
     def list_task_to_validated(self,new_value):
         self.__list_task_to_be_validated=new_value
 
+    ################ méthode permettant l'ajout et la suppression 
+
+    #prend un objet Task et l'ajoute a liste des taches 
+    #input : task => est un objet TASK !! 
+    def add_task(self,task):
+        self.list_task.append(task)
+
+    def add_reward(self,reward):
+        self.list_reward.append(reward)
+    
+    def add_task_history(self,task_history):
+        self.list_task_history.append(task_history)
+    
+    def add_task_to_be_validated(self,task_history):
+        self.list_task_to_validated.append(task_history)
+    
+    def add_children(self,children):
+        self.list_children.append(children)
+
+    ##### ici input reward + nom de l'enfant
+    def add_reward_to_be_granted(self,reward,children_name):
+        self.list_reward_to_be_granted.append([reward,children_name])
+
+
+    ###### delete 
+
+    def del_task(self, index):
+        if 0 <= index < len(self.list_task):
+            del self.list_task[index]
+    
+    def del_reward(self, index):
+        if 0 <= index < len(self.list_reward):
+            del self.list_reward[index]
+    
+    def del_task_history(self, index):
+        if 0 <= index < len(self.list_task_history):
+            del self.list_task_history[index]
+    
+    def del_task_to_be_validated(self, index):
+        if 0 <= index < len(self.list_task_to_validated):
+            del self.list_task_to_validated[index]
+    
+    def del_children(self, index):
+        if 0 <= index < len(self.list_children):
+            del self.list_children[index]
+    
+    def del_reward_to_be_granted(self, index):
+        if 0 <= index < len(self.list_reward_to_be_granted):
+            del self.list_reward_to_be_granted[index]
+
+    ######update 
+
+    def update_task(self, index, new_task):
+        if 0 <= index < len(self.list_task):
+            self.list_task[index] = new_task
+    
+    def update_reward(self, index, new_reward):
+        if 0 <= index < len(self.list_reward):
+            self.list_reward[index] = new_reward
+    
+    #inutile normalement
+    def update_task_history(self, index, new_task_history):
+        if 0 <= index < len(self.list_task_history):
+            self.list_task_history[index] = new_task_history
+    
+    def update_children(self, index, new_children):
+        if 0 <= index < len(self.list_children):
+            self.list_children[index] = new_children
+    
+
+    ### Inutile noramlement
+    def update_reward_to_be_granted(self, index, new_reward_data):
+        if 0 <= index < len(self.list_reward_to_be_granted):
+            self.list_reward_to_be_granted[index] = new_reward_data
+
+
+
+    ######
+
     def load_data(self):
         self.list_task=read_task_file()
         self.list_children=read_children_file()
