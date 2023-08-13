3d8dce755757462da43c45f3f57352953a552921
Parfondry <64077936+Parfondry@users.noreply.github.com>
2023-08-13 04:31:03 +0200

    ajout class task et taskHistory

--- task.py
+++ task.py
@@ -1,61 +1,30 @@
 from datetime import *
 
 class Task:
-    current_id=0
-
-    #l'id s'incrémente à chaque nouvel instance créer de telle manière que chaque instance a un id unique
-    def __init__(self, desc,last_time,limite_time,point):
+    
+    def __init__(self, name, point, last_time_done, time_limit):
+        self.name = name
         date_format = "%Y-%m-%d"
-        self.__desc =desc
-        self.__point = point
-        self.__last_time = datetime.strptime(last_time, date_format).date()
-        self.__limite_time = datetime.strptime(limite_time, date_format).date()
+        self.point = point
+        self.last_time_done =  datetime.strptime(last_time_done, date_format).date()
+        self.time_limit = datetime.strptime(time_limit, date_format).date()
 
-    @property
-    def desc(self):
-        return self.__desc
 
     @property
     def point(self):
-        return self.__point
-    
-    
-    @property
-    def last_time(self):
-        return self.__last_time
-    
-    @property
-    def limite_time(self):
-        return self.__limite_time
-    
-
-    
-    @last_time.setter
-    def last_time(self,new_last_time):
-        self.__last_time=new_last_time
-    
-    @limite_time.setter
-    def limite_time(self,new_limite_time):
-        self.__limite_time=new_limite_time
+        return self.point    
     
     @point.setter
-    def point(self,new_point):
-        self.__frequency=new_point
-    
-    @desc.setter
-    def desc(self,new_desc):
-        self.__desc=new_desc
+    def point(self, new_point):
+        self.point = new_point
 
+    
 
 
     #fonction qui permet de transformer un objet TASK en un dictionaire.
-    def transform_to_dico(self):
-        dico={'desc':self.desc,'last_time':self.last_time.strftime("%Y-%m-%d"),'limite_time':self.limite_time.strftime("%Y-%m-%d"),'point':self.point}
-        return dico
+   # def transform_to_dico(self):
+    #    dico={'id':self.id,'desc':self.desc,'last_time':self.last_time.strftime("%Y-%m-%d"),'limite_time':self.limite_time.strftime("%Y-%m-%d"),'frequency':self.frequency,'done':self.done}     
+     #   return dico
 
-    def __repr__(self):
-        return(' desc : ' + str(self.desc))
-
-
-    
-    
+    #def __repr__(self):
+     #   return('id : ' + str(self.id) + ' desc : ' + str(self.desc))
--- task_history.py
+++ task_history.py
@@ -1,47 +1,41 @@
 from datetime import *
+
+
 class TaskHistory:
-    current_id = 0
-    def __init__(self, description_task, date_done, user_did):
-        TaskHistory.current_id += 1
+    #current_id = 0
+    def __init__(self, name , children, date_done):
+        #TaskHistory.current_id += 1
         date_format = "%Y-%m-%d"
-        self.__id = TaskHistory.current_id
-        self.__description_task = description_task
+        #self.__id = TaskHistory.current_id
+        self.__name = name
         self.__date_done = datetime.strptime(date_done, date_format).date()
-        self.__user_did = user_did
+        self.__children = children
     
-    @property
-    def id(self):
-        return self.__id
+    #@property
+    #def id(self):
+     #   return self.__id
 
     @property
-    def description_task(self):
-        return self.__description_task
+    def name(self):
+        return self.__name
 
     @property
     def date_done(self):
         return self.__date_done
 
-    @property
-    def user_did(self):
-        return self.__user_did
-
-    @id.setter
-    def id(self,new_id):
-        self.__id=new_id
+#    @id.setter
+ #   def id(self,new_id):
+  #      self.__id=new_id
     
-    @description_task.setter
-    def description_task(self,new_description_task):
-        self.__description_task=new_description_task
+    @name.setter
+    def name(self,new_name):
+        self.__name=new_name
     
     @date_done.setter
     def date_done(self,new_date_done):
         self.__date_done=new_date_done
 
-    @user_did.setter
-    def user_did(self,new_user_did):
-        self.__user_did=new_user_did
-
     #fonction qui permet de transformer un objet TASK en un dictionaire.
-    def transform_to_dico(self):
-        dico={'id':self.id,'description_task':self.description_task,'date_done':self.date_done.strftime("%Y-%m-%d"),'user_did':self.user_did}     
-        return dico
+   # def transform_to_dico(self):
+      #  dico={'id':self.id,'description_task':self.description_task,'date_done':self.date_done.strftime("%Y-%m-%d"),'user_did':self.user_did}     
+       # return dico
