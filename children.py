aad03123d0f7886ca0211ce150f3216d9b1ed658
Parfondry <64077936+Parfondry@users.noreply.github.com>
2023-08-13 16:14:38 +0200

    class children

--- children.py
+++ children.py
@@ -1,54 +1,5 @@
-
-
-class Children:
-
-    ongoing_tasks = [
-    {"task": "Do homework", "date": "2023-08-12"},
-    {"task": "Clean room", "date": "2023-08-10"},
-    {"task": "Read a book", "date": "2023-08-11"}
-    
-    ]
-    def __init__(self, name, type, points):
-        super().__init__(name, type)
-        self.points = points
-        self.ongoing_tasks = ongoing_tasks
-
-    def mark_task_as_completed(self):
-        if not self.ongoing_tasks:
-            print("No ongoing tasks available.")
-            return
-
-        print("Ongoing tasks:")
-        for index, task_info in enumerate(self.ongoing_tasks, start=1):
-            task = task_info["task"]
-            date = task_info["date"]
-            print(f"{index}. {task} (Date: {date})")
-
-        task_choice = input("Enter the number of the task you want to go: ")
-        try:
-            task_choice = int(task_choice)
-            if 1 <= task_choice <= len(self.ongoing_tasks):
-                task_to_complete = self.ongoing_tasks[task_choice - 1]["task"]
-                print(f"Do you want to mark '{task_to_complete}' as completed?")
-                choice = input("1. Yes\n2. No\nYour choice: ")
-
-                if choice == "1":
-                    print(f"Task '{task_to_complete}' marked as completed.")
-                    self.ongoing_tasks.pop(task_choice - 1)
-                elif choice == "2":
-                    print("Task not marked as completed.")
-                else:
-                    print("Invalid choice.")
-            else:
-                print("Invalid task number.")
-        except ValueError:
-            print("Invalid input. Please enter a valid number.")
-
-
-
-
-
-""" from user import User
+from user import User
+from datamanager import DataManager
 
 class Children(User):
     def __init__(self, name, type, point, reward_list):
@@ -77,11 +28,12 @@ class Children(User):
 
 
     def request_task(self, task, name):
-        pass
+        
 
-    def request_reward(self, name, date):
-        pass
+    def request_reward(self, reward, child_name):
+        DataManager.add_reward_to_be_granted(reward, child_name)
+
     
-    def transform_to_dico(self):
-        dico={'name':self.name,'point':self.point,'reward_list':self.reward_list}
-        return dico """
+    #def transform_to_dico(self):
+     #   dico={'name':self.name,'point':self.point,'reward_list':self.reward_list}
+      #  return dico 
